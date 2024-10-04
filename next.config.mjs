452b3b94c368f95b9a68ab2b72de1e3782e741d0
Timothy Adeyemi <tadeyemi45@gmail.com>
2024-10-04 16:26:34 -0400

    tims page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+// /** @type {import("next").NextConfig} */
+// const nextConfig = {
+//   output: "export",
+// };
+
+// export default nextConfig;
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // Disable Image Optimization for static export compatibility
+  },
 };
 
 export default nextConfig;
